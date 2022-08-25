f7c2101497d14640a64271af6a6310754bb45615
Mariangel86 <Mariangel_antequera@hotmail.com>
2022-08-24 23:23:03 -0500

    new store repo

---
+++ src/componentes/Tienda.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tienda = () => {
+    return (
+      
+              <div>
+                <h1> Tienda</h1>
+                <p>aqui estara todo el contenido</p>
+              </div> 
+              
+    );
+    
+  }
+  
+  export default Tienda;
